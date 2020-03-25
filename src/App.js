6ef3d61b12b3b97738b32ee825357e70e08d985c
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-03-25 14:40:28 -0700

    refactored app component page view conditional

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
 import Header from './Header/Header'
+import Footer from './Footer/Footer'
 
 const App = () => {
 
   const [view, setView] = useState('home')
 
+  let page = null;
+
   if (view === 'home') {
-    return (
-      <div className="App">
-        <Header setView={setView} />
-        <h1>HOMEPAGE</h1>
-      </div>
-    );
+    page = <h1>HOMEPAGE</h1>
   } else if (view === 'about') {
-    return (
-      <div className="App">
-        <Header setView={setView} />
-        <h1>ABOUT</h1>
-      </div>
-    );
+    page = <h1>ABOUT</h1>
   } else if (view === 'projects') {
-    return (
-      <div className="App">
-        <Header setView={setView} />
-        <h1>PROJECTS</h1>
-      </div>
-    );
+    page = <h1>PROJECTS</h1>
   } else if (view === 'contact') {
-    return (
-      <div className="App">
-        <Header setView={setView} />
-        <h1>CONTACT</h1>
-      </div>
-    );
+    page = <h1>CONTACT</h1>
   }
+
+  return (
+    <div className="App">
+      <Header setView={setView} />
+      {page}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a target="blank" href="https://github.com/huntertread">GH</a>
+      <a target="blank" href="https://www.linkedin.com/in/huntertread/">LI</a>
+    </div>
+  )
+}
+
+export default Footer
