c6294d4d04b3c3c9d8a40141074cf089e642ae5a
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-03-30 16:22:03 -0700

    updated projects text color and homepage intro text

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -4,9 +4,7 @@ import './homepage.css'
 const Homepage = () => {
   return (
     <div id="home-section" className="homepage">
-      <h1>...WELCOME TO</h1>
-      <h1>HUNTER</h1>
-      <h1>TREADAWAY</h1>
+      <h1>...WELCOME TO<br/>HUNTER<br/>TREADAWAY</h1>
       <h2>q h</h2>
     </div>
   )
