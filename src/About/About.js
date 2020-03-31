36471de344ea542c034343404c914f9557f237cb
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-03-30 17:26:04 -0700

    cropped about img and added to local, reworked about skill area

--- src/About/About.js
+++ src/About/About.js
@@ -5,11 +5,13 @@ const About = () => {
   return (
     <div id="about-section" className="about">
       <h1>ABOUT</h1>
-      <p>hunter is a human from earth who likes to make things</p>
-      <p>React | React-Native | Viro-React | Styled-Components | D3 | HTML | CSS | JavaScript | jQuery</p>
-      <p>Node</p>
+      <h2>hunter is a human from earth who likes to make things with:</h2>
+      <p>Javascript | HTML | CSS | python | bash</p>
+      <p>React | React-Native | Viro-React | Styled-Components | D3 | jQuery | node.js</p>
       <p>MySQL | PostgreSQL | MongoDB</p>
-      <img src="https://huntertreadportfolio.s3-us-west-1.amazonaws.com/GestationCrate.png" alt="human sow nurses her young in a gestation crate" />
+      <p>windows | linux | mac</p>
+      <p>spray-paint</p>
+      <img src={require("../img/GestationCrateCrop.png")} alt="human sow nurses her young in a gestation crate" />
     </div>
   )
 }
