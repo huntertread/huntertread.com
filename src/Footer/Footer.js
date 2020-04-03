5286fb08f61ace06103566f48f356849ddc8fb45
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-04-03 10:14:53 -0700

    stubbed out projects section accordion placeholders

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
 
   return (
     <div className="footer">
-      <a target="blank" alt="link to github profile" href="https://github.com/huntertread" className="fa fa-github" />
-      <a target="blank" alt="link to linked-in profile"  href="https://www.linkedin.com/in/huntertread/" className="fa fa-linkedin" />
-      <a target="blank" alt="link to instagram profile" href="https://www.instagram.com/protozoasaur/?hl=en" className="fa fa-instagram" />
+      <a target="blank" alt="link to github profile" href="https://github.com/huntertread" className="fa fa-github" ></a>
+      <a target="blank" alt="link to linked-in profile"  href="https://www.linkedin.com/in/huntertread/" className="fa fa-linkedin" ></a>
+      <a target="blank" alt="link to instagram profile" href="https://www.instagram.com/protozoasaur/?hl=en" className="fa fa-instagram" ></a>
     </div>
   )
 }
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -3,6 +3,12 @@ import AccordionDrawer from '../AccordionDrawer/AccordionDrawer'
 import './projects.css'
 
 const Projects = () => {
+  
+  const htContent = 'this website right here! font is sourced from a classic NES game called Holy Diver'
+  const htLink = ''
+
+  const threeContent = 'a fancy three.js project'
+  const threeLink = ''
 
   const ecommContent = 'a sampling of e-commerce components in the neumorphic design style'
   const ecommLink = 'https://github.com/huntertread/e-comm-components'
@@ -14,6 +20,8 @@ const Projects = () => {
     <div id="project-section" className="projects">
       <h1>PROJECTS</h1>
       <div className="projects-content">
+        <AccordionDrawer title={'huntertreadaway.com'} content={htContent} link={htLink} />
+        <AccordionDrawer title={'unknown three.js projects'} content={threeContent} link={threeLink} />
         <AccordionDrawer title={'e-comm components'} content={ecommContent} link={ecommLink} />
         <AccordionDrawer title={'system design capstone'} content={sysDisContent} link={sysDisLink} />
       </div>
