9d8edf8321c378517627cb0643542fbb9232a75f
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-04-03 08:00:27 -0700

    added social icons

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
 
   return (
     <div className="footer">
-      <a target="blank" href="https://github.com/huntertread">GH</a>
-      <a target="blank" href="https://www.linkedin.com/in/huntertread/">LI</a>
-      <a target="blank" href="https://www.instagram.com/protozoasaur/?hl=en">IG</a>
+      <a target="blank" alt="link to github profile" href="https://github.com/huntertread" className="fa fa-github" />
+      <a target="blank" alt="link to linked-in profile"  href="https://www.linkedin.com/in/huntertread/" className="fa fa-linkedin" />
+      <a target="blank" alt="link to instagram profile" href="https://www.instagram.com/protozoasaur/?hl=en" className="fa fa-instagram" />
     </div>
   )
 }
