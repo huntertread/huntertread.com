777058b778a18018ef756c888f126c9c52ce350a
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-04-04 17:01:40 -0700

    refactored accordion component to allow for animated open/close icon

--- src/AccordionDrawer/AccordionDrawer.js
+++ src/AccordionDrawer/AccordionDrawer.js
@@ -1,29 +1,45 @@
 import React, { useState } from 'react'
 import './accordiondrawer.css'
 
+let counter = 0;
+
 const AccordionDrawer = ({title, content, link}) => {
 
   const [isOpen, setIsOpen] = useState(false)
+  const [iconState, setIconState] = useState('closed')
 
-  if (isOpen === false) {
-    return (
-      <div className="accordion-header">
-        <p>{title}</p>
-        <span onClick={() => setIsOpen(!isOpen)}>+</span>
-      </div>
-    )
-  } else if (isOpen === true) {
-    return (
-      <div className="accordion-drawer-open">
-        <div className="accordion-header">
-          <p>{title}</p>
-          <span onClick={() => setIsOpen(!isOpen)}>x</span>
-        </div>
+  let accContent = null;
+
+  const onClickOpen = () => {
+    if (counter % 2 === 0) {
+      setIsOpen(!isOpen)
+      setIconState('open')
+      counter++
+    } else {
+      setIsOpen(!isOpen)
+      setIconState('closed')
+      counter++
+    }
+  }
+
+  if (isOpen === true) {
+    accContent =
+      <div className="accordion-content">
         <p>{content}</p>
         <a className="project-cta" href={link} target="blank" alt={`go to ${title} source code or live example`}>Go to content source</a>
       </div>
-    )
   }
+
+  return (
+    <div>
+    <div className="accordion-header">
+      <p>{title}</p>
+      <i id={iconState} className="fa fa-plus" onClick={onClickOpen}></i>
+    </div>
+      <div>{accContent}</div>
+    </div>
+
+  )
 }
 
 export default AccordionDrawer
