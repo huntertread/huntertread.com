f2fe82bb4b7bd845ede5da580328a450e5a7e4a6
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-04-08 10:50:42 -0700

    added CRT css effect, need to update to cover entire page instead of only home

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="crt">
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
