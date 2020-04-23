281c0fdb0ebf343b98c191679a5c624482df6915
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-04-23 16:27:27 -0700

    Updates

---
+++ precache-manifest.c4ffe3c65acb85afea9eac7bf789dd0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c315f93c908d916c65a7afe4dbf268",
+    "url": "/index.html"
+  },
+  {
+    "revision": "756a76be03085e42778e",
+    "url": "/static/css/main.2e44e256.chunk.css"
+  },
+  {
+    "revision": "a131a5ba3c5576add86b",
+    "url": "/static/js/2.99ef6950.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/static/js/2.99ef6950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756a76be03085e42778e",
+    "url": "/static/js/main.1d32e81b.chunk.js"
+  },
+  {
+    "revision": "4f45a3abb237a8bc0fdd",
+    "url": "/static/js/runtime-main.d22ddcc3.js"
+  },
+  {
+    "revision": "8690fc679ee841ee35025dcbd8d2a44d",
+    "url": "/static/media/mononokekeh.8690fc67.ttf"
+  },
+  {
+    "revision": "84b12948a70cb1d4871ac61c736b45aa",
+    "url": "/static/media/xevious.84b12948.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-site/precache-manifest.a5db267c7d07cbd214916db6a6874a02.js"
+  "/precache-manifest.c4ffe3c65acb85afea9eac7bf789dd0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-site/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
