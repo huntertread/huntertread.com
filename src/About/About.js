60e5c7416f65290ee0afb308d3cd5187e46104cf
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-04-23 17:39:14 -0700

    updated about section

--- src/About/About.js
+++ src/About/About.js
@@ -6,11 +6,12 @@ const About = () => {
     <main role="main" id="about-section" className="about">
       <h1>ABOUT</h1>
       <div className="about-content">
-        <h3 className="about-headline">Hunter is a web developer residing in Los Angeles, CA. He aims to surprise users with unexpected interactions and design choices. He enjoys working with:</h3>
-        <p>JavaScript | HTML5 | CSS3</p>
+        <h3 className="about-headline">Hunter is a web developer in Los Angeles, CA. He enjoys building unique interactive interfaces with test-driven, performant, and modular code.</h3>
+        <p>JavaScript | HTML5 | CSS3 | Bash | PHP</p>
         <p>React | React-Native | ViroReact | Styled-Components | D3 | jQuery | Node.js</p>
+        <p>Jest | Mocha | Chai</p>
         <p>MySQL | PostgreSQL | MongoDB</p>
-        <p>Windows | Linux | Mac</p>
+        <p>AWS EC2 | S3 | Route53</p>
       </div>
       <a href="./downloadFiles/Hunter_Treadaway_Software_Engineering_Resume.pdf" download>Download Resume</a>
       <p alt="a custom font is used which transforms alpha-numeric characters in to pixel monsters" className="character-container">yuyuy</p>
