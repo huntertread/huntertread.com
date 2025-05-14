302577ff0cbfb4ff31016d57095f03b48e17e8ad
Hunter Treadaway <huntertreadaway@hunters-mbp.mynetworksettings.com>
2025-05-14 12:32:27 -0400

    adds homepage sub title: The PM You've Been Looking For

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -5,6 +5,7 @@ const Homepage = () => {
   return (
     <main role="main" id="home-section" className="homepage">
       <h1>HUNTER<br/>TREADAWAY</h1>
+      <h3>The PM You've Been Looking For</h3>
       <p alt="a custom font is used which transforms alpha-numeric characters in to pixel monsters" className="character-container">q h</p>
     </main>
   )
