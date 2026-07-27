964afe42ad5230669ed1450829eef7816a58c31b
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2026-07-27 16:54:06 -0400

    updates home page headline and about page content. cleans up old commented code

--- src/About/About.js
+++ src/About/About.js
@@ -7,26 +7,14 @@ const About = () => {
       <h1>ABOUT</h1>
       <div className="about-content">
         <h3 className="about-headline">Hunter is a Creative Project Manager in Metro NYC.</h3>
-        {/* <p>JavaScript | HTML5 | CSS3 | Bash</p>
-        <p>React | Redux | Node | jQuery</p>
-        <p>Styled-Components | SASS | SCSS</p>
-        <p>Jest | Enzyme | Mocha | Chai</p>
-        <p>MySQL | PostgreSQL | MongoDB</p>
-        <p>AWS EC2 | S3 | Route53</p> */}
+        <p>He bridges the gap between creative vision and technical execution, helping teams transform ideas into digital products and experiences.</p>
         <p>Creative Project Manager | Digital Product Delivery | Cross-Functional Operations</p>
-        <p>Certified Scrum Master. Fluent in Agile, Scrum, Waterfall</p>
-        <p>Jira, Asana, Trello. Comfortable with any PM tool</p>
-        <p>5 Years Experience in Digital Advertising</p>
-        <p>5 Years Experience in FinTech</p>
-        <p>10 years managing or working in development teams</p>
-        <p>10 years delivering in high pressure environments</p>
-        {/* <p>Hunter is calmer than a Fruitger Aero playlist</p> */}
-        {/* <p>Yes, he IS a project manager but he's also so much more</p>
-        <p>He is a <a href="https://www.instagram.com/protozoasaur/" target="blank">multi-media artist</a> with front end development experience</p>
-        <p>Hunter likes to do it all, he doesnt want to be put in a box</p>
-        <p>Hunter wants to break free of the norm</p>
-        <p>Hunter is calmer than a Frutiger Aero playlist</p>
-        <p>He's also super kind and easy to work with (check out his LI refs)</p> */}
+        <p>Certified ScrumMaster experienced in Agile, Scrum, and hybrid delivery environments.</p>
+        <p>10+ years partnering with designers, developers, QA teams, and business stakeholders.</p>
+        <p>5 years in digital advertising.</p>
+        <p>5 years in FinTech.</p>
+        <p>10 years delivering complex projects in fast-paced environments.</p>
+        <p>Experienced with Jira, Asana, Trello, and modern product workflows.</p>
       </div>
       <a href="./downloadFiles/Hunter Treadaway - Creative Project Manager.pdf" target="blank" >Resume</a>
       <p alt="a custom font is used which transforms alpha-numeric characters in to pixel monsters" className="character-container">55</p>
--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -5,7 +5,8 @@ const Homepage = () => {
   return (
     <main role="main" id="home-section" className="homepage">
       <h1>HUNTER<br/>TREADAWAY</h1>
-      <h3>The PM You've Been Looking For</h3>
+      <h3>Creative Project Manager</h3>
+      <h4>Bridging creative vision, technology, and execution.</h4>
       <p alt="a custom font is used which transforms alpha-numeric characters in to pixel monsters" className="character-container">&nbsp;</p>
     </main>
   )
