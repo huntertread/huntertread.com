ef176b28affda3caea651f58df0f4e4ad6589337
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2026-07-29 15:54:53 -0400

    Updates

---
+++ precache-manifest.d2ea35c7130452e0ff6bed28e318a304.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565e397b43bd5a9f3e728f04e692ad27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60f04d219c11c5c49be1",
+    "url": "/static/css/main.5572ac68.chunk.css"
+  },
+  {
+    "revision": "7019159c05c995adf038",
+    "url": "/static/js/2.5c446588.chunk.js"
+  },
+  {
+    "revision": "5bdacc385fa6e96a4d70fd13632c4407",
+    "url": "/static/js/2.5c446588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f04d219c11c5c49be1",
+    "url": "/static/js/main.e629726c.chunk.js"
+  },
+  {
+    "revision": "22dbdc99d966f7142c13",
+    "url": "/static/js/runtime-main.98ffbd2d.js"
+  },
+  {
+    "revision": "7ab9c15b188eb58affa26d091179bf47",
+    "url": "/static/media/aztec.7ab9c15b.jpg"
+  },
+  {
+    "revision": "372a9950066e49a2f696387a52499f4c",
+    "url": "/static/media/finalsculpt.372a9950.jpg"
+  },
+  {
+    "revision": "a908d114b22982c6f558e21d052a5dc0",
+    "url": "/static/media/glowfinal.a908d114.jpg"
+  },
+  {
+    "revision": "a5276434a43596bb5fcd2f8a658364fe",
+    "url": "/static/media/homer.a5276434.jpg"
+  },
+  {
+    "revision": "8690fc679ee841ee35025dcbd8d2a44d",
+    "url": "/static/media/mononokekeh.8690fc67.ttf"
+  },
+  {
+    "revision": "4797a80c32695819a550da207ff895b3",
+    "url": "/static/media/original.4797a80c.jpg"
+  },
+  {
+    "revision": "ac0b7bdc61797dd9f4cd7dcbdd406d8d",
+    "url": "/static/media/pmg.ac0b7bdc.png"
+  },
+  {
+    "revision": "cd5ee5d01d0115c58a9f6a48396daac5",
+    "url": "/static/media/prewax.cd5ee5d0.jpg"
+  },
+  {
+    "revision": "232cffabf47a82998ea38a72557c6006",
+    "url": "/static/media/prototype.232cffab.jpg"
+  },
+  {
+    "revision": "1e7fe9eb892a58e9c03f9d3f81c46611",
+    "url": "/static/media/readyship.1e7fe9eb.jpg"
+  },
+  {
+    "revision": "6dbd5e8e94309f1571943cbf7eee87c1",
+    "url": "/static/media/rsahi.6dbd5e8e.png"
+  },
+  {
+    "revision": "aa061285cc31b07a4be6a6b32d9c9910",
+    "url": "/static/media/tinmold.aa061285.jpg"
+  },
+  {
+    "revision": "2ab45cceb3762baa0526e6989962e208",
+    "url": "/static/media/waxhead.2ab45cce.jpg"
+  },
+  {
+    "revision": "84b12948a70cb1d4871ac61c736b45aa",
+    "url": "/static/media/xevious.84b12948.ttf"
+  }
+]);
